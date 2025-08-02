053b1d8c0248bba02d4dc8dd6fb399c393e32eea
Javaria Masooma <124299551+j-masuma@users.noreply.github.com>
2025-08-02 18:34:26 +0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import HeroSection from '../components/homePageComponents/Herosection'
+import HeroSection from '../components/homePageComponents/HeroSection'
 import Hire from '../components/homePageComponents/Hire'
 import CSection from '../components/homePageComponents/CSection'
 import BSection from '../components/homePageComponents/BSection'
 import DSection from '../components/homePageComponents/DSection'
 import {Performance} from '../components/homePageComponents/Performance'
-import BreakSection from '../components/homePageComponents/BreakSection'
+// import BreakSection from '../components/homePageComponents/BreakSection'
 
 function HomePage() {
   return (
     <>
       <HeroSection/>
       <BSection/>
-      <BreakSection/>
+{/*       <BreakSection/> */}
       <Performance/>
       <CSection/>
       <DSection/>
